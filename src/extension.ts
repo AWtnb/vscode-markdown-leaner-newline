364cd38828d7434d97c631fc57bd667f3ed918bc
awtnb <arikaebanataw@gmail.com>
2024-03-25 22:29:10 +0900

    update more simply

--- src/extension.ts
+++ src/extension.ts
@@ -1,51 +1,61 @@
 import * as vscode from "vscode";
 
 import { EditorLine } from "./EditorLine";
-import { CursorSelection } from "./CursorSelection";
 
 const newlineToCursors = (editor: vscode.TextEditor) => {
   const linebreak = editor.document.eol === 1 ? "\n" : "\r\n";
-  editor.edit((editBuilder: vscode.TextEditorEdit) => {
-    editor.selections.forEach((sel: vscode.Selection) => {
-      const el = new EditorLine(editor, sel);
-      const cs = new CursorSelection(sel, editBuilder);
-      const symbol = el.getSymbol();
+  const workspaceEdit = new vscode.WorkspaceEdit();
+  const uri = editor.document.uri;
 
-      if (!sel.isEmpty) {
-        const s = el.isCursorInsideIndent() ? el.getStringBeforeCursor() : el.getIndentWhitespace() + symbol;
-        cs.delete();
-        cs.insert(linebreak + s);
-        return;
+  editor.selections.forEach((sel: vscode.Selection) => {
+    const el = new EditorLine(editor, sel);
+    const symbol = el.getSymbol();
+
+    if (!sel.isEmpty) {
+      const s = el.isCursorInsideIndent() ? el.getStringBeforeCursor() : el.getIndentWhitespace() + symbol;
+      workspaceEdit.replace(uri, sel, linebreak + s);
+      return;
+    }
+
+    if (el.isCursorInsideIndent()) {
+      workspaceEdit.insert(uri, sel.start, linebreak + el.getStringBeforeCursor());
+      return;
+    }
+
+    if (el.hasText()) {
+      workspaceEdit.insert(uri, sel.start, linebreak + el.getIndentWhitespace() + symbol);
+      return;
+    }
+
+    if (symbol.length < 1) {
+      workspaceEdit.insert(uri, sel.start, linebreak + el.getIndentWhitespace());
+      return;
+    }
+
+    if (el.isQuotation()) {
+      workspaceEdit.insert(uri, sel.start, linebreak + el.getIndentWhitespace() + symbol);
+      return;
+    }
+
+    const lineRange = editor.document.lineAt(sel.start).range;
+    if (el.isIndented()) {
+      const o = el.getOutdentedText(Number(editor.options.indentSize));
+      workspaceEdit.replace(uri, lineRange, o);
+      return;
+    }
+
+    workspaceEdit.replace(uri, lineRange, "");
+  });
+
+  vscode.workspace.applyEdit(workspaceEdit).then(() => {
+    const newSels = editor.selections.map((sel) => {
+      if (sel.isEmpty) {
+        return sel;
       }
-
-      if (el.isCursorInsideIndent()) {
-        cs.insert(linebreak + el.getStringBeforeCursor());
-        return;
-      }
-
-      if (el.hasText()) {
-        cs.insert(linebreak + el.getIndentWhitespace() + symbol);
-        return;
-      }
-
-      if (symbol.length < 1) {
-        cs.insert(linebreak + el.getIndentWhitespace());
-        return;
-      }
-
-      if (el.isQuotation()) {
-        cs.insert(linebreak + el.getIndentWhitespace() + symbol);
-        return;
-      }
-
-      if (el.isIndented()) {
-        const o = el.getOutdentedText(Number(editor.options.indentSize));
-        cs.replaceLine(editor, o);
-        return;
-      }
-
-      cs.replaceLine(editor, "");
+      const s = new vscode.Selection(sel.end, sel.end);
+      return s;
     });
+    editor.selections = newSels;
   });
 };
 
